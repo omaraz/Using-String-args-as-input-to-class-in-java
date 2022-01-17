7a9f8ea8e6999a9341ef4bf2e296fc00da862b07
omaraz <37741507+omaraz@users.noreply.github.com>
2022-01-17 23:31:17 +0200

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -1,22 +1,27 @@
-import java.util.Scanner;
+import java.util.Scanner; //to use scanner to get data from the user
+/* our objective is to pass data 
+while call the code to runusing cmd
+*/
 
+//the class name with the main must be the same as file name
 class Example1{
-	public static void main(String args[]){
+	public static void main(String args[]){ 
+		//create a new scanner
 		Scanner scan = new Scanner(System.in);
 		
 		//System.out.println("Omar Aziz");
 		//String name = scan.nextLine();
 		
-		if(args != null){
-			System.out.println("The name you entered: "+ args[0]);
-			int userint = Integer.parseInt(args[1]);
-			for(int i = 0 ; i<userint ; i++){
+		if(args != null){//check if the array of strings is not empty
+			System.out.println("The name you entered: "+ args[0]); //print Firststring
+			int userint = Integer.parseInt(args[1]); //we use the second string as integer so change its type to integer
+			for(int i = 0 ; i<userint ; i++){ //loop with it to print empty line to print the third input after these number of lines 
 			System.out.print("\n");				
 
 			}
-			System.out.printf("You enter number = %d \n %s",userint ,args[2]);
+			System.out.printf("You enter number = %d \n %s",userint ,args[2]); //print the third argument
 		}else
-			System.out.println("The input is empty!!");
+			System.out.println("The input is empty!!");//if it called and no parameters passed
 		/*System.out.print("Enter a word: ");
 		String userinput = scan.nextLine();
 		System.out.print("Enter the line differance = ");
